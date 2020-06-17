d2616f21a1a2325598c4503bdf97900f25ea5a12
Fliko <baxter.flw@gmail.com>
2020-06-17 07:25:48 -0500

    refocus action

--- run.sh
+++ run.sh
@@ -1,46 +1,6 @@
-#!/bin/bash
-
-echo "Environment Variables"
-echo "REPO: ${REPO}"
-echo "USERNAME: ${USERNAME}"
-
-if [ "${GITHUB_REF}" = "master" ]
-then
-GITHUB_REF = "sandbox"
-fi
-
-echo "GITHUB_REF: ${GITHUB_REF}"
-echo "GITHUB_SHA: ${GITHUB_SHA}"
-
-echo "Configuring git"
-git config --global hub.protocol "https"
-git config --global url."https://${USERNAME}:${TOKEN}@github.com".insteadOf "https://github.com"
-git config --global user.email "${EMAIL}"
-git config --global user.name "${USER}"
-
-echo "Initialize GIT"
-git init
-
-echo "Cloning repo ${REPO}"
-git clone https://github.com/${REPO}.git 
-
-echo "Adding remote for repo ${REPO}"
-git remote add origin https://github.com/${REPO}.git
-
-echo "Checking out branch"
-echo EXTRACT_GITHUB_REF=${GITHUB_REF##*/}
-git fetch
-git checkout ${GITHUB_REF##*/}
-
-echo "setup npmrc"
-touch .npmrc
-echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > .npmrc
-cat .npmrc
-
-# echo "manually set registry"
-# npm set registry "http://registry.npmjs.org"
-# npm set //registry.npmjs.org/:_authToken $NPM_AUTH_TOKEN
-
-echo "Deploying Gateway"
-serverless config credentials --provider aws --key "${AWS_ACCESS_KEY}" --secret "${AWS_SECRET_ACCESS_KEY}"
-sls --msg "${GITHUB_REF}-${GITHUB_SHA}" --stage ${GITHUB_REF##*/} --debug
+#!/bin/sh
+# Using this script because github action passess the arguments as strings
+# instead of array
+echo ">>> Running sls $@"
+sls $@
+echo ">>> Compleated"

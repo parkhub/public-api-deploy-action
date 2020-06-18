3a70553f0e4c7038b6a2e442d75b5d24886187f6
Fliko <baxter.flw@gmail.com>
2020-06-18 12:06:13 -0500

    pin things down

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# Using this script because github action passess the arguments as strings
-# instead of array
-npm i @parkhub/aws-api-gateway @parkhub/config
 
-echo ">>> Running sls $@"
-
-sls $@
-
-echo ">>> Compleated"
+RUN npm i -g serverless@1.71.3
+ENTRYPOINT ["serverless"]

5e9f9c8b045614f482740652c9439e63fc22eb4e
Fliko <baxter.flw@gmail.com>
2020-06-18 10:29:28 -0500

    install parkhub npm manually

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Using this script because github action passess the arguments as strings
 # instead of array
+npm i @parkhub/aws-api-gateway @parkhub/config
+
 echo ">>> Running sls $@"
 ls -al node_modules
 sls $@
